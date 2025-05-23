038b183f89f7d0a0b06d00449468a59322eb2b7a
Sam Creviston <samuelcreviston@gmail.com>
2025-05-23 17:15:10 -0400

    built dtools and server.js setup

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 3000;
+
+// //api documentation UI
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger-output.json');
+// var options = {
+//   swaggerOptions: {
+//     validatorUrl: null
+//   }
+// };
+
+
+//connects to MogoDB by creating a class with a function (client) to create a session and make requests
+require('dotenv').config(); //loads environment variables from a `.env` file into `process.env`.
+const { MongoClient } = require('mongodb'); //MongoClient is a class used to connect to MongoDB database and run queries.
+
+const client = new MongoClient(process.env.MONGODB_URI); //creates a new MongoDB client using connection string from the `.env` file.
+async function connectDB() {
+  try {
+    await client.connect(); //attempt to connect to MongoDB
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+connectDB();
+module.exports = client; //exports the client object so other files (like route files) can import and use the database connection.
+
+
+//enable json reading for server
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/', require('./routes'));
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+//enable api documetation UI
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
